5501802187a8bd502a71114b363c35bf178d6ae7
steban9810 <esteban_9810@outlook.es>
2019-05-26 22:47:00 -0500

    quiz

--- signup.php
+++ signup.php
@@ -10,22 +10,49 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script>
     function comprobarClave(){
-   	  clave1 = document.f1.pswd.value
-   	  clave2 = document.f1.pswd2.value
-   	  if (clave1 == clave2)
+   	  password1 = document.f1.pswd.value
+   	  password2 = document.f1.pswd2.value
+   	  if (password1 == password2)
 		    document.f1.submit()
         else
-        alert("Las contraseñas son distintas")
+        alert("Digite la misma contraseña")
       }
-  </script>
 
+      function validartipofoto(){
+	  $max=2000000;// 2Mb)
+	  $filesize = $_FILES['upfile']['size'];
+	  $filename = trim($_FILES['upfile']['name']);
+	  $filename = substr($filename, -20);
+	  $filename = ereg_replace(" ", "", $filename);
+	if($filesize < $max){
+		if($filesize > 0){
+			if((ereg(".jpg", $filename)) || (ereg(".gif", $filename)) || (ereg(".JPG", $filename))|| (ereg(".GIF", $filename))){
+				$uploadfile = $uploaddir . $filename;
+				if (move_uploaded_file($_FILES['profiles']['tmp_name'], $uploadfile)) {
+				print("Archivo subido correctamente");
+				} else {
+				print("Error de conexi&oacute;n con el servidor.");
+				}
+				} else {
+				print("Sólo se permiten imágenes en formato jpg. y gif., no se ha podido adjuntar.");
+				}
+				}
+				else {
+				print("<br><br>Campo vac&iacute;o, no ha seleccionado ninguna imagen");
+				}
+				}
+				else {
+				print("<br><br>La imagen que ha intentado adjuntar es mayor de 1.5 Mb, si desea cambie el tamaño del archivo y vuelva a intentarlo.");
+		}
+  }
+  </script>
 </head>
 <body>
 
 <div class="container">
   <h2>Sign Up</h2>
   <p>Formulario de registro de usuarios</p>
-  <form action="uregister.php" class="was-validated" method="POST" name="f1">
+  <form action="uregister.php" class="was-validated" method="POST"  name="f1" enctype="multipart/form-data">
     <div class="form-group">
       <label for="uname">Fisrtname:</label>
       <input type="text" class="form-control" id="uname" placeholder="Enter Firstname" name="uname" required>
@@ -35,6 +62,13 @@
     <div class="form-group">
       <label for="uname">Lastname:</label>
       <input type="text" class="form-control" id="ulastname" placeholder="Enter Lastname" name="ulastname" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+
+    <div class="form-group">
+      <label for="uname">Nit:</label>
+      <input type="text" class="form-control" id="nit" placeholder="Enter ID" name="nit" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
@@ -55,22 +89,38 @@
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
+
+
+
     <div class="form-group">
       <label for="pwd">Password:</label>
       <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" required>
-      <div class="valid-feedback">Valid.</div>
+      <div class="help-block">Minimum of 6 characters</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
     <div class="form-group">
       <label for="pwd">Confirmar password:</label>
       <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd2" required>
+      <div class="help-block">Minimum of 6 characters</div>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
 
+    <div class="form-group">
+      <label for="photo">photo</label>
+      <input type="file" class="form-control" id="photo" name="photo" required>
+
+    </div>
+
+
+
     <button type="button" class="btn btn-primary"  onClick="comprobarClave()"  >Register</button>
   </form>
 </div>
+
+
+
+
 
 </body>
 </html>
